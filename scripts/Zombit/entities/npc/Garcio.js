3dd0524e1e2e155cf7825e32408d72e1bad2b17c
Parent Louis <louis.parent@etu.umontpellier.fr>
2019-03-27 15:47:54 +0100

    Removing <b> and <i> in dialog

--- scripts/Zombit/entities/npc/Garcio.js
+++ scripts/Zombit/entities/npc/Garcio.js
@@ -2,9 +2,9 @@ class Garcio extends MultiDialingNPC
 {
     constructor(owningState)
     {
-    	let toSay =["<center><big><b><i>Garcio</i></b> : <b>SAUVEZ MOI</b> ILS NE ME LACHENT PAS</big></center>", 
-    				"<center><big><b><i>Soldat</i></b> : Je suis la monsieur.</big></center>",
-    				"<center><big><b><i>Garcio</i></b> : Ils vont bientôt arriver, vite !</big></center>"];
+    	let toSay =["<center><big>Garcio : SAUVEZ MOI ILS NE ME LACHENT PAS</big></center>", 
+    				"<center><big>Soldat : Je suis la monsieur.</big></center>",
+    				"<center><big>Garcio : Ils vont bientôt arriver, vite !</big></center>"];
     				
         super(owningState, toSay, "Garcio", "assets/entities/npc/garcio/garcio.png", Layers.getLayer("background").getWidth()*0.35, Layers.getLayer("background").getHeight()*0.8, player.getWidth()*1.1, player.getWidth() * 1.1 * 1.3);
 
@@ -40,10 +40,10 @@ class Garcio extends MultiDialingNPC
         {
         	this.dialogLevel++;
         	
-        	this.dialingText = "<center><big><b><i>Garcio</i></b> : Merci <i>Soldat</i>.</big></center>";
-        	this.texts = ["<center><big><b><i>Soldat</i></b> : Monsieur j'ai besoin de votre aide pour progresser dans le batiment, toute les portes sont fermés.</big></center>",
-        				  "<center><big><b><i>Garcio</i></b> : Je suis le <b>directeur du département</b> je possède toute <b>les clés</b>. Je crois que certains de mes collegues sont encore dans leurs salle venez quand vous aurez récupéré votre arme. Je vous accompagner pour les aider.</big></center>",
-        				  "<center><big><b><i>Soldat</i></b> : Merci, allons y.</big></center>",
+        	this.dialingText = "<center><big>Garcio : Merci Soldat.</big></center>";
+        	this.texts = ["<center><big>Soldat : Monsieur j'ai besoin de votre aide pour progresser dans le batiment, toute les portes sont fermés.</big></center>",
+        				  "<center><big>Garcio : Je suis le directeur du département je possède toute les clés. Je crois que certains de mes collegues sont encore dans leurs salle venez quand vous aurez récupéré votre arme. Je vous accompagner pour les aider.</big></center>",
+        				  "<center><big>Soldat : Merci, allons y.</big></center>",
         				  ""];
         	
         }
